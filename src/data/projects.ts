5a42e0e5f97b8bcbd9351f5c956ca8f138d2f6b7
sunkeyfong <hi@sunkey.me>
2024-02-25 16:00:34 +0800

    A Projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,20 +7,16 @@ export type Project = {
 
 const projects: Project[] = [
   {
-    title: "Dictionary App",
-    techs: ["ReactJS (NextJS)", "react-query", "zod"],
-    link: "https://github.com/MaeWolff/dictionary-app",
+    icon: 'holli',
+    title: "Holli.cc / English learner's AI mate",
+    techs: ["ReactJS","Nodejs","Mysql"],
+    link: "https://holli.cc",
   },
   {
-    title: "Portfolio / Lina BLIDI",
-    techs: ["ReactJS (NextJS)", "TypeScript"],
-    link: "https://www.linablidi.fr/",
-  },
-  {
-    title: "Portfolio / Template",
-    techs: ["Astro"],
-    link: "/",
-    isComingSoon: true,
+    icon:'bejolly',
+    title: "边界旅行 / 本地营销收客神器",
+    techs: ["Trao(微信小程序)", "PHP","Mysql"],
+    link: "https://www.bjlx.top/",
   },
 ];
 
